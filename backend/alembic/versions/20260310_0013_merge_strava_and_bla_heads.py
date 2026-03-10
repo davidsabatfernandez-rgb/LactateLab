7b90d971b2a2d74c173cfcb99a635165ba3d4811
David <davisabatfernandez@icloud.com>
2026-03-10 19:13:48 +0100

    Fix Render deploy migrations

---
+++ backend/alembic/versions/20260310_0013_merge_strava_and_bla_heads.py
@@ -0,0 +1,19 @@
+"""merge Strava and bla_check migration heads
+
+Revision ID: 20260310_0013
+Revises: 20260310_0012, 974b1ced7ebb
+Create Date: 2026-03-10 19:05:00
+"""
+
+revision = "20260310_0013"
+down_revision = ("20260310_0012", "974b1ced7ebb")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
